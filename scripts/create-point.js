aa1c05b92fc1543b0f3792fb487a3c555fd30771
Bruno <bruno.souza2001@gmail.com>
2020-06-04 22:34:40 -0300

    Next Level Week #3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,11 +1,6 @@
-//     document
-//         .querySelector("select[name=uf]")
-//         .addEventListener("change", () => {
-//         console.log("Mudei")
-//  })      
-
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
+    
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
     .then( res => res.json())
     .then( states => {
@@ -13,13 +8,14 @@ function populateUFs() {
         for(const state of states ) {
         ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
         }
-    } )
+    })
 
 }
 
 populateUFs()
 
 function getCities(event){
+    
     const citySelect = document.querySelector("[name=city]")
     const stateInput = document.querySelector("[name=state]")
 
@@ -30,12 +26,16 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/distritos`
     const ufSelect = document.querySelector("select[name=uf]")
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json())
     .then( cities => {
         
         for(const city of cities ) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
         
     citySelect.disabled = false
@@ -48,3 +48,42 @@ function getCities(event){
         .addEventListener("change", getCities)
 
 
+// Itens de Coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+ }
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+    
+    //Adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId // Isso será true ou false
+        return itemFound
+    })
+
+if( alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter(item => {
+            const itemIsDiferrent = item != itemId
+            return itemIsDiferrent  
+    })
+selectedItems = filteredItems
+} else {
+    selectedItems.push(itemId)
+}
+
+collectedItems.value = selectedItems
+
+}

ddd156b66468a60aa3b10f2ae334fcc3b7e74c7f
Bruno <bruno.souza2001@gmail.com>
2020-06-07 22:40:00 -0300

    Next Level Week #5

---
+++ README.md
@@ -0,0 +1,30 @@
+
+<img width="30" src="public/assests/lampadas.svg"> 
+<img width="30" src="docs/icones/baterias.svg">
+<img width="30" src="docs/icones/papeis-papelao.svg">
+<img width="30" src="docs/icones/eletronicos.svg">
+<img width="30" src="docs/icones/organicos.svg">
+<img width="30" src="docs/icones/oleo.svg">
+<h2>Aplicação feita na Next Level Week #1 da @Rocketseat, nos dias 1 a 5 de Junho
+
+<img src="/public/assets/README/nlw.png" alt="nlw">
+
+<h3>Front-end:</h3>
+<p><a href="https://mozilla.github.io/nunjucks/">Nunjucks</a></br>
+<a href="https://html.spec.whatwg.org/multipage/">HTML 5</a></br>
+<a href="https://www.w3schools.com/Css/">CSS 3</a></br>
+<a href="https://www.javascript.com/">JavaScript</a></p>
+
+<h3>Back-end:</h3>
+<p><a href="https://nodejs.org/en/">NodeJS</a></br>
+<a href="https://expressjs.com/">Express</a></br>
+<a href="https://www.npmjs.com/package/nodemon">Nodemon</a></br>
+<a href="https://www.sqlite.org/index.html">SQLite3</a></p>
+
+<h3>APIs</h3>
+
+<p><a href="https://servicodados.ibge.gov.br/api/docs">IBGE</a></p>
+
+
+
+</h2>
---
+++ src/database/db.js
@@ -0,0 +1,80 @@
+const sqlite3 = require("sqlite3").verbose()
+
+const db = new sqlite3.Database("./src/database/database.db")
+
+module.exports = db
+
+// SQL
+// db.serialize(() => {
+    // com comandos SQL:
+
+    // criar uma tabela
+    // db.run(`
+    //     CREATE TABLE IF NOT EXISTS places (
+    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         image TEXT,
+    //         name TEXT,
+    //         address TEXT,
+    //         address2 TEXT,
+    //         state TEXT,
+    //         city TEXT,
+    //         items TEXT
+    //     );
+    // `)
+
+
+    // // inserir dados na tabela
+    // const query = `
+    // INSERT INTO places (
+    //     image,
+    //     name,
+    //     address,
+    //     address2,
+    //     state,
+    //     city,
+    //     items
+    // ) VALUES (?,?,?,?,?,?,?);
+    // `
+    // const values = [
+    //     "https://images.unsplash.com/photo-1503596476-1c12a8ba09a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    //     "Paperside",
+    //     "Guilherme Gemballa, Jardim América",
+    //     "Nº 260",
+    //     "Santa Cantarina",
+    //     "Rio do Sul",
+    //     "Resíduos Eletronicos, Lâmpadas"
+
+    // ]
+
+    // function afterInsertData(err) {
+    //     if (err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Cadastrado com sucesso")
+    //     console.log(this)
+    // }
+
+    // db.run(query, values, afterInsertData)
+
+    // consultar os dados da tabela
+    // db.all(`SELECT * FROM places`, function(err, rows){
+    //     if (err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Aqui estão seus registros: ")
+    //     console.log(rows)
+    // })
+
+
+    //deletar um dado da tabela
+
+    // db.run(`DELETE FROM places WHERE id = ?`, [1], function(err){
+    //     if (err) {
+    //         return console.log(err)
+    //     }
+    //     console.log("Registro deletado com sucesso")
+    // })
+
+//})
